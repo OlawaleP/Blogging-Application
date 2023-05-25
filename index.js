c21f89bdc8cca86ff5bb9404c2e2081339fc9253
OlawaleP <99323873+OlawaleP@users.noreply.github.com>
2023-05-25 09:47:57 +0100

    updated the index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser  = require('cookie-parser');
+
+//import  connectDB  from './config/database.config'
+
+require('dotenv').config()
+
+
+//import authorRouter from './routes/authors';
+
+
+
+//connectDB();
+
+const app = express();
+
+// Middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors);
+
+
+//Middleware
+
+//app.use('/author', authorRouter);
+
+//Port 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(` Server running on port ${port}`);
+})
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(error.status || 500);
+  res.render('error');
+});
+
+//export default app;
